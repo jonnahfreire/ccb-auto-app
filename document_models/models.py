3fab03ba843c96b5810917928c411a0d6516d56c
jonnahfreire <jonnas.freire17@gmail.com>
2022-03-30 01:31:32 -0300

    first commit

---
+++ document_models/models.py
@@ -0,0 +1,12 @@
+model3026 = {
+    "type": "",
+    "num": "",
+    "date": "",
+    "value": "",
+    "emitter": "",
+    "expenditure": "3026",
+    "hist-1": "021",
+    "hist-2": "023",
+    "cost-center": "ADM",
+    "cost-account": "1000"
+}
