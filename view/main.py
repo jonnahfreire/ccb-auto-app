ba9d7eb8519eed61228a3a7870a1797f782caa3b
jonnahfreire <jonnas.freire17@gmail.com>
2022-06-15 02:08:49 -0300

    Add screen classes / list views ...

--- view/main.py
+++ view/main.py
@@ -38,7 +38,6 @@ class HSeparator(QWidget):
         self.separator.setMaximumSize(width, height)
 
 
-
 class AlertModal(QWidget):
 
     def __init__(self, parent, width=400, height=300, title=None, body=None) -> None:
@@ -64,6 +63,7 @@ class AlertModal(QWidget):
         self.container.setFrameShape(QFrame.StyledPanel)
         self.container.setFrameStyle(QFrame.Raised)
         self.container.setStyleSheet(context_menu_style)
+        set_element_boxshadow(self.container)
         self.container.setMinimumSize(self.width, self.height)
         self.container.setMaximumSize(self.width, self.height)
 
@@ -96,12 +96,14 @@ class AlertModal(QWidget):
         self.modal_btn_ok = QLabel("OK", self.container)
         self.modal_btn_ok.setCursor(Qt.PointingHandCursor)
         self.modal_btn_ok.setStyleSheet(modal_btn_ok_style)
+        set_element_boxshadow(self.modal_btn_ok)
         self.modal_btn_ok.move(self.width - 150, self.height-40)
 
         self.modal_btn_cancel = QLabel("Cancelar", self.container)
         self.modal_btn_cancel.setCursor(Qt.PointingHandCursor)
         self.modal_btn_cancel.move(self.width - 90, self.height-40)
         self.modal_btn_cancel.setStyleSheet(modal_btn_cancel_style)
+        set_element_boxshadow(self.modal_btn_cancel)
         
         self.modal_btn_cancel.mousePressEvent = self.handle_btn_cancel
         self.modal_close.mousePressEvent = self.handle_btn_close
@@ -157,7 +159,9 @@ class AlertLoading(QWidget):
 
         self.alert_container.setMinimumSize(container_w, container_h)
         self.alert_container.setMaximumSize(container_w, container_h)
-        self.alert_container.setStyleSheet(context_menu_style if self.style is None else self.style)
+        self.alert_container.setStyleSheet(alert_loading_container_style if self.style is None else self.style)
+        set_element_boxshadow(self.alert_container)
+
         self.set_position(int(p_w/2)-int(container_w/2), int(p_h/2)-80-int(container_h/2))
 
         self.movie = QMovie("view/assets/loading-line.gif")
@@ -221,6 +225,8 @@ class ContextMenu(QWidget):
         self.context_menu_container.setMinimumSize(self.width, self.height)
         self.context_menu_container.setMaximumSize(self.width, self.height)
         self.context_menu_container.setVisible(False)
+        set_element_boxshadow(self.context_menu_container)
+
 
         self.context_menu_layout = QVBoxLayout(self.context_menu_container)
         self.set_position(150, 100)
@@ -254,6 +260,46 @@ class ContextMenu(QWidget):
     def hide(self):
         self.is_shown = False
         self.context_menu_container.hide()
+
+
+def set_element_boxshadow(element, blur=5,
+    x_offset=1.0, y_offset=1.0, offset=1):
+    
+    shadow = QGraphicsDropShadowEffect()
+    # setting blur radius
+    shadow.setBlurRadius(blur)
+    if x_offset != 1.0: shadow.setXOffset(x_offset)
+    if y_offset != 1.0: shadow.setYOffset(y_offset)
+    if x_offset == 1.0 and y_offset == 1.0: shadow.setOffset(offset)
+
+    # adding shadow to the label
+    element.setGraphicsEffect(shadow)
+    
+  
+class SettingsScreen(QDialog):
+
+    def __init__(self, parent) -> None:
+        super().__init__()
+        self.parent = parent
+        loadUi("view/ui/settings.ui", self)
+
+        self.back = self.backBtn
+        self.back.mousePressEvent = self.handle_back_click
+
+        self.info.setStyleSheet("""
+            background-color: rgb(245, 245, 245);
+            border-radius: 5px;
+            padding: 5px;
+            """)
+
+        set_element_boxshadow(self.info)
+        self.selectDriverPathBtn.setMinimumSize(135, 30)
+        self.selectDriverPathBtn.setStyleSheet(btn_success + "QPushButton{font-size: 10pt;}")
+
+        set_element_boxshadow(self.selectDriverPathBtn)
+
+    def handle_back_click(self, event):
+        self.parent.setCurrentIndex(self.parent.currentIndex() - 1)
 
 
 class UserCredentialScreen(QDialog):
@@ -305,15 +351,14 @@ class MainScreen(QDialog):
     def __init__(self, parent) -> None:
         super().__init__()
         self.parent = parent
+        self.folders = []
 
         loadUi("view/ui/main-screen.ui", self)
 
-        self.mousePressEvent = self.handle_mouse_press
-        self.status_container = self.leftStatusFrame
+        self.appHeader.setStyleSheet(app_header_bg)
 
-        self.folder = self.folder
-        self.folder.mousePressEvent = self.folder_clicked
-        self.folder.setStyleSheet(folder_style)
+        self.parent.mousePressEvent = self.handle_mouse_press
+        self.status_container = self.leftStatusFrame
 
         self.folder_context = ContextMenu(self)
         self.folder_context.add_item("Abrir Local", self.handle_open_folder_location)
@@ -324,7 +369,7 @@ class MainScreen(QDialog):
         self.perfil_context.set_position(650, 50)
         self.perfil_context.add_item("Resetar Usuário")
         self.perfil_context.add_separator()
-        self.perfil_context.add_item("Configurações")
+        self.perfil_context.add_item("Configurações", self.handle_settings_view)
 
         self.add_extract_option = ContextMenu(self, 120, 40)
         self.add_extract_option.add_item("Adicionar Extrato", self.handle_add_extract)
@@ -345,14 +390,17 @@ class MainScreen(QDialog):
 
         self.createDirBtn.clicked.connect(self.create_dir_clicked)
         self.createDirBtn.setStyleSheet(btn_primary)
+        set_element_boxshadow(self.createDirBtn)
 
         self.addItemsBtn.clicked.connect(self.add_items_clicked)
         self.addItemsBtn.mousePressEvent = self.add_items_clicked
         self.addItemsBtn.setStyleSheet(btn_primary)
+        set_element_boxshadow(self.addItemsBtn)
 
         self.startInsertionBtn.clicked.connect(self.start_insertion_clicked)
         self.startInsertionBtn.mousePressEvent = self.start_insertion_clicked
         self.startInsertionBtn.setStyleSheet(btn_success)
+        set_element_boxshadow(self.startInsertionBtn)
 
         self.userName.mousePressEvent = self.handle_perfil_clicked
         self.userIcon.mousePressEvent = self.handle_perfil_clicked
@@ -367,7 +415,36 @@ class MainScreen(QDialog):
 
         p = lambda: print("Response modal: True")
         self.modal.on_ok_press(p)
+
+        self.workingMonthHeader.setStyleSheet(main_view_headings_style)
+        self.debtReceivingsHeader.setStyleSheet(main_view_headings_style)
+        set_element_boxshadow(self.workingMonthHeader)
+        set_element_boxshadow(self.debtReceivingsHeader)
+
+        self.folder_list_view = ListView(self.listMonthContainer, 770, 70, 0, 2)
+        self.folder_list_view.hide_scrollbar()
+        self.folder_list_view.set_horizontal()
+        self.folder_list_view.set_content_padding(5)
+
+        for month in ["05/2022", "06/2022"]:
+            folder = FolderItem(self.folder_list_view, month)
+            folder.click(self.folders, self.handle_folder_callback)
+            self.folder_list_view.add_item(folder)
+            self.folders.append(folder)
+            
+
+        list_view_widget = ListView(self.itemsContainer, 770, 315, 0, 0)
+        list_view_widget.hide_scrollbar()
+
+        # for __ in [0, 1, 2]:
+        list_item = InsertionItem()
+        list_view_widget.add_item(list_item)
     
+    def handle_settings_view(self, event):
+        settings_view = SettingsScreen(self.parent)
+        self.parent.addWidget(settings_view)
+        self.parent.setCurrentIndex(self.parent.currentIndex() + 1)
+
     def handle_perfil_clicked(self, event):
         if event.buttons() == Qt.LeftButton:
             self.perfil_context.show()
@@ -378,13 +455,11 @@ class MainScreen(QDialog):
         self.folder_context.hide()
         self.perfil_context.hide()
     
-    def folder_clicked(self, event):
+    def handle_folder_callback(self, event, folder):
         if event.buttons() == Qt.RightButton:
             self.folder_context.show()
-            
-        self.folder.setStyleSheet(folder_style_active)
-        self.folderIcon.setPixmap(QPixmap(u"view/assets/folderOpen.png"))
-        print(self.folderMonthTitle.text())            
+        print("callback folder click: ", folder.month_text.text())
+                   
 
     def create_dir_clicked(self):
         pass
@@ -438,7 +513,159 @@ class MainScreen(QDialog):
             self.status_not_started.hide()
             self.status_finished.hide()
 
+class InsertionItem(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
         
+        self.container = QVBoxLayout()
+        self.container.setContentsMargins(0,0,0,0)
+        self.setLayout(self.container)
+
+
+        self.account1000_container = QFrame()
+        self.account1010_container = QFrame()
+        self.extract_container = QFrame()
+
+        self.account1000_header = QLabel("Caixa Geral - 1000", self.account1000_container)
+        self.account1010_header = QLabel("Banco - 1010", self.account1010_container)
+        self.extract_header = QLabel("Extrato", self.extract_container)
+
+        self.container.addWidget(self.account1000_header, 0)
+        self.container.addWidget(self.account1010_header, 2)
+        self.container.addWidget(self.extract_header, 3)
+
+        self.item_container = QHBoxLayout()
+        self.item_container.setSpacing(10)
+        self.item_container.setAlignment(Qt.AlignLeft)
+
+        self.item_icon = QLabel()
+        self.icon("view/assets/file-earmark-pdf.png")
+        self.item_icon.setMaximumSize(30, 30)
+
+        self.items = []
+
+        self.item_container.addWidget(self.item_icon, 0)
+        for index, item in enumerate(["NF 922","12/06/2022","R$ 120,00","DP 3026","Não lançado"], 1):
+            item = QLabel(item)
+
+            item.setStyleSheet(insertion_item_commom_style)
+            set_element_boxshadow(item)
+
+            item.setAlignment(Qt.AlignCenter)
+            item.setLayoutDirection(Qt.RightToLeft)
+            item.setCursor(Qt.PointingHandCursor)
+
+            item.setMinimumSize(100, 30)
+            item.setMaximumSize(100, 30)
+
+            self.item_container.addWidget(item, index)
+            self.items.append(item)
+
+        self.container.addLayout(self.item_container)
+
+    def icon (self, icon):
+        self.item_icon.setStyleSheet(insertion_item_commom_style)
+        self.item_icon.setScaledContents(True)
+        self.item_icon.setMargin(4)
+        self.item_icon.setPixmap(QPixmap(icon).scaled(30, 30))# 
+
+
+class FolderItem(QWidget):
+    def __init__(self, parent, month=None) -> None:
+        super().__init__()
+        self.parent = parent
+
+        self.container = QFrame()
+        self.container.setStyleSheet(folder_style)
+        self.container.setMinimumSize(115, 55)
+        self.container.setMaximumSize(115, 55)
+        self.container.setCursor(Qt.PointingHandCursor)
+
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0,0,5,0)
+        self.layout.addWidget(self.container)
+        self.setLayout(self.layout)
+
+        self.folder_icon = QLabel(self.container)
+        self.folder_icon.setStyleSheet(folder_icon_style)
+        self.folder_icon.move(11, 9)
+        self.folder_icon.setMinimumSize(38, 38)
+        self.folder_icon.setMaximumSize(38, 38)
+        self.folder_icon.setScaledContents(True)
+        self.folder_icon.setPixmap(QPixmap("view/assets/folder.png"))
+        set_element_boxshadow(self.container, offset=1)
+
+        self.month_text = QLabel(month, self.container)
+        self.month_text.setStyleSheet(folder_text_style)
+        self.month_text.move(55, 14)
+        self.month_text.setMinimumSize(55, 30)
+        self.month_text.setMaximumSize(55, 30)
+    
+    def set_folder_open(self):
+        self.container.setStyleSheet(folder_style_active)
+        self.folder_icon.setPixmap(QPixmap("view/assets/folder-open.png"))
+    
+    def set_folder_closed(self):
+        self.container.setStyleSheet(folder_style)
+        self.folder_icon.setPixmap(QPixmap("view/assets/folder.png"))
+
+    def set_month_text(self, text):
+        self.month_text.setText(text)
+    
+    def check_btn_clicked(self, folders, event, callback):
+        if event.buttons() == Qt.RightButton:
+            callback(event, self)
+        else:
+            for folder in folders:
+                folder.set_folder_closed()
+            self.set_folder_open()
+            callback(event, self)
+
+    def click(self, folders, callback):
+        self.container.mousePressEvent = lambda e: self.check_btn_clicked(folders, e, callback)
+
+
+class ListView(QListView):
+
+    def __init__(self, parent, width=100, height=100, x=0, y=0) -> None:
+        super().__init__()
+        self.parent = parent
+
+        self.list_view_widget = QListWidget(self.parent)
+        self.list_view_widget.setSelectionMode(QAbstractItemView.NoSelection)
+        self.list_view_widget.setGeometry(x, y, width, height)
+
+        self.list_view_widget.setFocusPolicy(Qt.NoFocus)
+
+        self.list_view_widget.setStyleSheet(list_view_style)
+    
+    def set_style(self, style):
+        self.list_view_widget.setStyleSheet(style)
+
+    def hide_scrollbar(self):
+        scrollbar = QScrollBar(self.parent)
+        scrollbar.setStyleSheet("width: 0px; height: 0px;")
+        self.list_view_widget.setHorizontalScrollBar(scrollbar)
+        self.list_view_widget.setVerticalScrollBar(scrollbar)
+
+    def set_content_padding(self, pd):
+        self.list_view_widget.setSpacing(pd)
+
+    def set_horizontal(self):
+        self.list_view_widget.setFlow(QListView.LeftToRight)
+        self.list_view_widget.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
+    
+    def set_vertical(self):
+        self.list_view_widget.setFlow(QListView.TopToBottom)
+        self.list_view_widget.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+
+    def add_item(self, item):
+        list_item = QListWidgetItem(self.list_view_widget)
+        list_item.setSizeHint(item.sizeHint())
+        self.list_view_widget.addItem(list_item)
+        self.list_view_widget.setItemWidget(list_item, item)
+
+
 class Window(QStackedWidget):
 
     def __init__(self) -> None:
@@ -452,23 +679,22 @@ class Window(QStackedWidget):
         self.setFixedHeight(600)
 
         self.alert_loading = AlertLoading(
-            self, p_w=800, p_h=600, infinity=False, duration=4000,
+            self, p_w=800, p_h=600, infinity=False, duration=1000,
             container_w=240, message="Iniciando, aguarde..", 
-            on_animation_end=self.load_default_screen)
-        
-        self.alert_loading.set_style("""
-            QFrame {background-color: rgba(221, 221, 221, 0.801);border-radius: 5px;}""")
-        
+            on_animation_end=self.load_views)
         self.alert_loading.start_animation()
 
-    def load_default_screen(self):
-        is_user_setted = False
+
+    def load_views(self):
+        is_user_setted = True
         if not is_user_setted:
-            self.user_credential_screen = UserCredentialScreen(self)
-            self.addWidget(self.user_credential_screen)
+            self.user_credential_view = UserCredentialScreen(self)
+            self.addWidget(self.user_credential_view)
         else:
-            self.main_screen = MainScreen(self)
-            self.addWidget(self.main_screen)   
+            self.main_view = MainScreen(self)
+            self.addWidget(self.main_view)
+            self.settings_view = SettingsScreen(self)
+            self.addWidget(self.settings_view)
 
     def load_window(self) -> None:
         self.setGeometry(self.left, self.top, self.width, self.height)
